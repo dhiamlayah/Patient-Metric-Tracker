f89b0734b286ad21f5160935e6e1e3c1036deb63
dhiamlayah <mlayahdhia8@gmail.com>
2025-01-24 14:28:33 +0100

    install typeorm and connect with database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MatricA1cModule } from './matric-a1c/matric-a1c.module';
+import { PatientModule } from './patient/patient.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [MatricA1cModule,PatientModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '2001',
+      username: 'postgres',
+      entities: [],
+      database: 'patient',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
